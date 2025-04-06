262624d8383750a34c8956fc9f96e627da157785
aayush-rajput <iaayushrajput@gmail.com>
2025-04-06 16:17:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -230,7 +230,7 @@ export default function Home() {
               transition={{ duration: 0.6, delay: 0.6 }}
             >
               <Button className="bg-gradient-to-r from-[#1E0D73] to-[#3D2A9C] hover:from-[#3D2A9C] hover:to-[#1E0D73] text-white text-lg px-8 py-7 rounded-xl group relative overflow-hidden">
-                <span className="relative z-10 " href="https://bookeasy-app.vercel.app" >Get Started</span>
+                <span className="relative z-10 " onclick="location.href='https://bookeasy-app.vercel.app';" >Get Started</span>
                 <motion.div
                   className="absolute inset-0 bg-gradient-to-r from-[#FF9800] to-[#FF9800]/80 z-0"
                   initial={{ x: "-100%" }}
